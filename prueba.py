da12a8e0bb9c7e191fb3842319091b8faaef836c
Alexmat18 <jefers.garcia@teczion.edu.gt>
2025-08-19 19:15:13 -0600

    Se cre la Base de datos para la palabras

--- prueba.py
+++ prueba.py
@@ -1,1 +1,14 @@
-print('Esta es una prueba  de github ')
+from sqlite3 import *
+from tkinter import messagebox
+#se hace la conexion con la base de datos
+baseDeDatos=connect("palabras.db")
+#se crea el cursor
+cr=baseDeDatos.cursor()
+#se crea la tabla
+cr.execute('''
+    CREATE TABLE IF NOT EXISTS palabras (
+           id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+           palabra TEXT NOT NULL, 
+           descripcion TEXT NOT NULL
+    )''')
+
