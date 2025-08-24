f4467f0493ecd21c23d6c585ab05323f2b61f61b
Alexmat18 <jefers.garcia@teczion.edu.gt>
2025-08-24 13:28:20 -0600

    Aqui usé un toplevel para hacer que las palabras de la base de datos se puedan ver en una ventana distinta a la principal

--- agregar_palabra.py
+++ agregar_palabra.py
@@ -1,12 +1,33 @@
 from sqlite3 import * 
-from tkinter import messagebox, Button
+from tkinter import messagebox
 from tkinter import *
+from tkinter import ttk #libreria para trabajar con tablas
 
 #----------------------------------------------
-#lista para almacenar la palabra
-listaDePlabras=[]
-#lista para almacenar las descripciones
-descripciones=[]
+#funcion para ver las palabras de la base de datos
+def verPalabras():
+    # Crear ventana emergente
+    ventanaTabla = Toplevel(app)
+    ventanaTabla.title("Palabras registradas")
+    ventanaTabla.geometry("600x300")
+
+    # Crear tabla Treeview
+    tabla = ttk.Treeview(ventanaTabla, columns=("ID","Palabra", "Descripcion"), show="headings")
+    tabla.heading("ID", text="ID")
+    tabla.heading("Palabra", text="Palabra")
+    tabla.heading("Descripcion", text="Descripción")
+    tabla.pack(fill="both", expand=True)
+
+    # Scroll vertical
+    scroll = ttk.Scrollbar(ventanaTabla, orient="vertical", command=tabla.yview)
+    tabla.configure(yscrollcommand=scroll.set)
+    scroll.pack(side="right", fill="y")
+
+    # Llenar la tabla con datos
+    cr.execute('SELECT * FROM palabras')
+    palabras = cr.fetchall()
+    for palabra in palabras:
+        tabla.insert("", "end", values=palabra)
 #---------------------------------------------
 # funcion para agregar la palabra
 def agregarPalabra():
@@ -14,13 +35,8 @@ def agregarPalabra():
     nuevaPalabra = str(nombrePalabra.get())
     nuevaDescripcion = descripcion.get("1.0", "end")
     #se reconfigura la etiqueta de cada caja
-    resultad2.configure(text=f"El valor es {nuevaDescripcion}")
-    resultado.configure(text=f"El valor es {nuevaPalabra}")
-    #agregaLas palabras a las listas cada vez que le toca el boton de agregar
-    listaDePlabras.append(nuevaPalabra)
-    descripciones.append(nuevaDescripcion)
-    print(listaDePlabras)
-    print (descripciones)
+    resultad2.configure(text=f"La palabra es {nuevaDescripcion}")
+    resultado.configure(text=f"Descripcion: {nuevaPalabra}")
     cr.execute('''
         INSERT INTO palabras (palabra, descripcion)
         VALUES (?,?)''', (nuevaPalabra, nuevaDescripcion))
@@ -37,37 +53,37 @@ app=Tk()
 app.title("Agregar Palabra")
 
 #se crea el tamaño de la ventana
-app.geometry("400x400")
-app.resizable(False,False)
+#app.geometry("500x400")
+#app.resizable(False,False)
+#---------------------------------------------------
+#
 #----------------------------------------------
 #etiqueta para dar la bienvenida
-etiqueta1=Label(app, text="Agregue una palabra nueva", bg="lightgreen", fg="blue", font=(25))
+etiqueta1=Label(app, text="Agregue una palabra nueva", font=(25))
 etiqueta1.grid(row=0, column=0, sticky="wens", columnspan=4)
 #etiqueta y entry para la palbra
-etiqueta2=Label(app, text='Ingrese la palabra: ', bg='lightgreen', fg='blue', font=(20))
+etiqueta2=Label(app, text='Ingrese la palabra: ', fg='blue', font=(20))
 etiqueta2.grid(row=1, column=0, sticky='wens', columnspan=2)
 nombrePalabra=Entry(app)
 nombrePalabra.grid(row=1, column=2, sticky='wens', columnspan=4)
 #etiqueta para la descripcion
-etiqueta3=Label(app, text='Ingrese la descripcion: ', bg='lightgreen', fg='blue', font=(20))
+etiqueta3=Label(app, text='Ingrese la descripcion: ', fg='blue', font=(20))
 etiqueta3.grid(row=2, column=0, sticky='wens', columnspan=2)
 descripcion=Text(app, height=5, width=30)
 descripcion.grid(row=2, column=2, sticky='wens', columnspan=3, pady=5, padx=3)
 #------------------------------------------------
 #varibles para almacenar las palabras y la descripcion
+resultado=Label(app, text="La palabra es: ")
+resultad2=Label(app, text="La descripcion es: ")
 
-
-resultado=Label(app, text="El valor es: ")
-
-resultad2=Label(app, text="El valor es: ")
-
-resultado.grid(row=3, column=0)
+resultado.grid(row=3, column=0, columnspan=3)
 resultad2.grid(row=4, column=0, columnspan=3)
-
 #------------------------------------------------------
 #boton de agregar
 resul=Button(app, text="Agregar", command=agregarPalabra)
 resul.grid(row=5, column=0)
-
+#boton para ver las palabras
+ver=Button(app, text="Ver Palabras", command=verPalabras)
+ver.grid(row=5, column=1)
 
 app.mainloop()
