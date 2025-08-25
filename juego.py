d4ca41ecef06d3042ef25e022b2bb12a2577f65d
carlos4090k <estudiantev@COED.LOCAL>
2025-08-25 16:15:57 -0600

    se esta solucionando un problema

--- juego.py
+++ juego.py
@@ -3,6 +3,15 @@ from sqlite3 import *
 from tkinter import messagebox
 from random import *
 
+numero = randint(1, 7)
+
+def jugar():
+        if valor1 == palabra:
+            messagebox.showinfo("felicidades ganaste", f"la palabra es: {palabra}")
+
+        else:
+            messagebox.showinfo("Lastima perdiste", f"la palabra es: {palabra}")
+
 base_de_datos = connect("palabras.db")
 cr = base_de_datos.cursor()
 
@@ -13,11 +22,8 @@ app.resizable(width=False,height=False)
 caja = Entry(app)
 caja.grid(row = 0, column = 0, padx=5, pady=5)
 
-numero = randint(1, 7)
-
-
-
-descripcion = cr.execute('''SELECT descripcion FROM palabras where id = ?''', (numero,))
+cr.execute('''SELECT descripcion FROM palabras WHERE id = ? ''', (numero,))
+descripcion =  cr.fetchall()
 
 etiqueta = Label(app, text="Descripcion")
 etiqueta.grid(row=1, column=0, padx=5, pady=5)
@@ -25,16 +31,13 @@ etiqueta.grid(row=1, column=0, padx=5, pady=5)
 texto = Label(app, text=f"{descripcion}")
 texto.grid(row=2, column=0, padx=5, pady=5)
 
-i = 0
-while True:
-    palabra = cr.execute('''SELECT palabra FROM palabras where id = ?''', (numero,))
-    valor1 = caja.get()
-    if palabra == valor1:
-        messagebox.showinfo("felicidades ganaste", f"la palabra es: {palabra}")
-        break
-    elif i == 3:
-        messagebox.showinfo("Lastima perdiste", f"la palabra es: {palabra}")
-        break
-    i = +1
+
+cr.execute('''SELECT palabra FROM palabras WHERE id = ?''', (numero,))
+palabra = cr.fetchall()
+
+valor1 = caja.get()
+
+for i in range(0, 3):
+    jugar()
 
 app.mainloop()
