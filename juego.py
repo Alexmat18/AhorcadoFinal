3de4a95d6da9561be5b5f2ac5673e6a5d6e45960
carlos4090k <estudiantev@COED.LOCAL>
2025-08-25 14:58:19 -0600

    se agrego,etiquet y la caja de descripcion

--- juego.py
+++ juego.py
@@ -10,7 +10,17 @@ cr = base_de_datos.cursor()
 
 def juego():
     numero = randint(1, 9)
+
     palabra = cr.execute('''SELECT palabra FROM palabras where id = ?''',(numero,))
+
+    descripcion=cr.execute('''SELECT descripcion FROM palabras where id = ?''',(numero,))
+
+    etiqueta = Label(app,text = "Descripcion")
+    etiqueta.grid(row = 1,column=0,padx = 5, pady = 5)
+
+    texto = Label(app, text = f"{descripcion}")
+    texto.grid(row = 2,column=0,padx = 5, pady = 5)
+
     i = 0
     while True:
         valor1 = caja.get()
@@ -19,6 +29,7 @@ def juego():
             break
         elif i == 3:
             messagebox.showinfo("Lastima perdiste",f"la palabra es: {palabra}")
+            break
         i = +1
 
 app = Tk()
@@ -27,3 +38,5 @@ app.resizable(width=False,height=False)
 
 caja = Entry(app)
 caja.grid(row = 0, column = 0)
+
+app.mainloop()
