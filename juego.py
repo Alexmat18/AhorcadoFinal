e96504494e6e54abbefbb0e0e0f2084c61ca292a
carlos4090k <estudiantev@COED.LOCAL>
2025-08-27 02:15:51 -0600

    se esta solucionando el problema de los intentos

--- juego.py
+++ juego.py
@@ -4,16 +4,24 @@ from tkinter import messagebox
 from random import *
 
 numero = randint(1, 7)
-
+i = 3
+estado = True
 def jugar():
-    valor1 = caja.get()
-    print(valor1)
-    if valor1 == dato:
-            messagebox.showinfo("felicidades ganaste", f"la palabra es: {dato}")
-    else:
-            messagebox.showinfo("Lastima perdiste", f"la palabra es: {dato}")
-            print(valor1)
 
+        valor1 = caja.get()
+        print(valor1)
+        if valor1 == dato:
+                messagebox.showinfo("felicidades ganaste", f"la palabra es: {dato}")
+        else:
+                estado = False
+        if estado == False:
+                messagebox.showinfo("ops", f"te quedan {i-1} intentos")
+                valor1 = caja.get()
+                caja.delete(0,END)
+
+
+print(estado)
+print(i)
 base_de_datos = connect("palabras.db")
 cr = base_de_datos.cursor()
 
@@ -22,7 +30,8 @@ app.title("AHORCADO")
 app.resizable(width=False,height=False)
 
 caja = Entry(app)
-caja.grid(row = 0, column = 0, padx=5, pady=5)
+caja.grid(row=0, column=0, padx=5, pady=5)
+
 
 cr.execute('''SELECT descripcion FROM palabras WHERE id = ? ''', (numero,))
 descripcion =  cr.fetchall()
@@ -39,9 +48,9 @@ palabra = cr.fetchall()
 dato = palabra[0][0]
 print(dato)
 
-
 btn = Button(app, text="Jugar", command=jugar)
 btn.grid(row=4, column=0, padx=5, pady=5)
+
 
 
 
