4dd09f76b35f098a1bb542e920b6542006116b41
Alexmat18 <jefers.garcia@teczion.edu.gt>
2025-08-27 02:15:43 -0600

    Se crearon los botones, pero los marcos no funcionan aun

--- Interfaz_principal.py
+++ Interfaz_principal.py
@@ -1,11 +1,22 @@
 from tkinter import *
+#funcion para cerrar la ventana principal
+def salir():
+    app.destroy()
 
 app=Tk()
+app.title("INTERFAZ PRINCIPAL")
 
-
-
-
-
-
-
-app.mainlooop()
+#creacion de  los marcos
+marco1=Frame(app,bg='lightYellow', width=100, height=100)
+marco2=Frame(app,bg='lightpink', width=100, height=10)
+marco1.grid(row=0, column=0, columnspan=3)
+marco1.grid(row=1, column=0, columnspan=3)
+#creacion de los botones para realizar acciones
+botonJugar=Button(marco1, text="Jugar")
+botonAcciones=Button(marco1, text="Acciones")
+botonSalir=Button(marco1, text="Salir", command=salir)
+#invocar los botones
+botonSalir.grid(row=0, column=0, pady=5, padx=5)
+botonJugar.grid(row=0, column=1, pady=5, padx=5)
+botonAcciones.grid(row=0, column=2, pady=5, padx=5)
+app.mainloop()
